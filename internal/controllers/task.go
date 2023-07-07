82cb79eed567ecb816567d0b3030990888c20946
Josh Santos <josh@omnidapps.com>
2023-07-07 18:29:53 -0400

    Extract model logic

--- internal/controllers/task.go
+++ internal/controllers/task.go
@@ -3,12 +3,8 @@ package controllers
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os/exec"
-	"strings"
 
 	"github.com/thatnerdjosh/example-webservices/internal/config"
 	"github.com/thatnerdjosh/example-webservices/internal/models"
@@ -50,46 +46,17 @@ func (t TaskController) ExecuteTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	b, _ := io.ReadAll(r.Body)
-	defer r.Body.Close()
 	// if err != nil {
 	// 	// Error processing request (e.g., timeout), return 500
 	// 	http.Error(w, err.Error(), 500)
 	// 	return
 	// }
 
-	// TODO: Extract logic to models
 	var task models.TaskRequest
-	err = json.Unmarshal(b, &task)
-	if task.Name == "" {
-		err = fmt.Errorf("malformed request, received %v", task)
-		log.Println(err)
-	}
+	_ = task.Process(t.config, r)
+	// if err != nil {
 
-	if err != nil {
-		badRequest(w)
-		return
-	}
-
-	taskItem := t.config.GetTask(task.Name)
-	if taskItem == nil {
-		// Task not found. Return 404
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"error":   "Task not found",
-			"success": false,
-		})
-		return
-	}
-
-	// NOTE: typically not recommended, but should be ok since it is driven entirely by a config file (no user input).
-	cmdParts := strings.Split(taskItem.Command, " ")
-	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
-
-	_, err = cmd.Output()
-	if err != nil {
-		fmt.Println("could not run command: ", err)
-	}
+	// }
 
 	w.WriteHeader(http.StatusOK)
 
@@ -97,29 +64,4 @@ func (t TaskController) ExecuteTask(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"success": true,
 	})
-}
-
-func badRequest(w http.ResponseWriter) {
-	// Data wasn't able to unmarshal, return with 400
-	w.WriteHeader(http.StatusBadRequest)
-
-	// TODO: Add error translation layer
-	json.NewEncoder(w).Encode(map[string]interface{}{
-		"error":   "Malformed request",
-		"success": false,
-	})
-}
-
-func forbidden(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusForbidden)
-	json.NewEncoder(w).Encode(map[string]interface{}{
-		"error":   "Not authenticated",
-		"success": false,
-	})
-}
-
-func authenticated(r *http.Request) bool {
-	// Authentication
-	// TODO: HTTP request to auth server
-	return r.Header.Get("Authorization") != ""
 }
--- internal/models/task.go
+++ internal/models/task.go
@@ -1,5 +1,66 @@
 package models
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+
+	"github.com/thatnerdjosh/example-webservices/internal/config"
+)
+
 type TaskRequest struct {
 	Name string `json:"name"`
+}
+
+func (t *TaskRequest) Process(cfg *config.TaskConfig, r *http.Request) error {
+	b, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	err := t.load(b)
+	if err != nil {
+		return err
+	}
+
+	return t.run(cfg)
+}
+
+func (t *TaskRequest) load(input []byte) error {
+	err := json.Unmarshal(input, &t)
+	if t.Name == "" {
+		err = fmt.Errorf("malformed request, received %v", t)
+		log.Println(err)
+	}
+
+	return err
+}
+
+func (t TaskRequest) run(cfg *config.TaskConfig) error {
+	taskItem := cfg.GetTask(t.Name)
+	if taskItem == nil {
+		return errors.New("")
+		// Task not found. Return 404
+		// w.WriteHeader(http.StatusNotFound)
+		// json.NewEncoder(w).Encode(map[string]interface{}{
+		// 	"error":   "Task not found",
+		// 	"success": false,
+		// })
+		// return
+	}
+
+	// NOTE: typically not recommended, but should be ok since it is driven entirely by a config file (no user input).
+	cmdParts := strings.Split(taskItem.Command, " ")
+	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
+
+	_, err := cmd.Output()
+	if err != nil {
+		fmt.Println("could not run command: ", err)
+		return err
+	}
+
+	return nil
 }
