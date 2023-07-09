937109eeaf7e65e5cf9f50ef041f2210b7de27f6
Josh Santos <josh@omnidapps.com>
2023-07-09 19:19:08 -0400

    Fix tests

--- internal/models/task.go
+++ internal/models/task.go
@@ -17,6 +17,11 @@ type TaskRequest struct {
 	Name string `json:"name"`
 }
 
+var (
+	ErrTaskNotFound = errors.New("task was not found")
+	ErrBadData      = errors.New("malformed data provided")
+)
+
 func (t *TaskRequest) Process(cfg *config.TaskConfig, r *http.Request) error {
 	b, _ := io.ReadAll(r.Body)
 	defer r.Body.Close()
@@ -32,7 +37,7 @@ func (t *TaskRequest) Process(cfg *config.TaskConfig, r *http.Request) error {
 func (t *TaskRequest) load(input []byte) error {
 	err := json.Unmarshal(input, &t)
 	if t.Name == "" {
-		err = fmt.Errorf("malformed request, received %v", t)
+		err = ErrBadData
 		log.Println(err)
 	}
 
@@ -42,14 +47,10 @@ func (t *TaskRequest) load(input []byte) error {
 func (t TaskRequest) run(cfg *config.TaskConfig) error {
 	taskItem := cfg.GetTask(t.Name)
 	if taskItem == nil {
-		return errors.New("")
-		// Task not found. Return 404
-		// w.WriteHeader(http.StatusNotFound)
-		// json.NewEncoder(w).Encode(map[string]interface{}{
-		// 	"error":   "Task not found",
-		// 	"success": false,
-		// })
-		// return
+		err := ErrTaskNotFound
+		log.Println(err)
+		return err
+
 	}
 
 	// NOTE: typically not recommended, but should be ok since it is driven entirely by a config file (no user input).
